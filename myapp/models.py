c683a7b2856207732742de6c7e4a297dbbaffa04
Marcos Malaquias <malaquiazmarcos@gmail.com>
2024-11-27 09:03:12 -0300

    todo las reservas se guardan correctamente en la base de datos

--- myapp/models.py
+++ myapp/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
 class Canchas(models.Model):
@@ -18,7 +19,7 @@ class Horario(models.Model):
     def __str__(self):
         return f'{self.fecha} - {self.horarios}'
 
-class DatosPersona(models.Model):
+""" class DatosPersona(models.Model):
     nombre = models.CharField(max_length=50)
     apellido = models.CharField(max_length=50)
     telefono = models.CharField(max_length=15)
@@ -27,5 +28,31 @@ class DatosPersona(models.Model):
     cancha = models.ForeignKey(Canchas, on_delete=models.CASCADE, related_name='persona')
 
     def __str__(self):
-        return f'{self.nombre} {self.apellido} - {self.telefono} - {self.email}'
+        return f'{self.nombre} {self.apellido} - {self.telefono} - {self.email}' """
+    
+
+""" class DatosPersona2(models.Model):
+    telefono = models.CharField(max_length=15)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    horario = models.ForeignKey(Horario, on_delete=models.CASCADE, related_name='persona')
+    cancha = models.ForeignKey(Canchas, on_delete=models.CASCADE, related_name='persona')
+
+    def __str__(self):
+        return f'{self.telefono}' """
+
+class DatosReserva(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    horario = models.ForeignKey(Horario, on_delete=models.CASCADE, related_name='persona')
+    cancha = models.ForeignKey(Canchas, on_delete=models.CASCADE, related_name='persona')
+
+    def __str__(self):
+        return f'{self.user} - {self.horario} - {self.cancha}'
+    
+class Telefono(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    telefono = models.CharField(max_length=15)
+
+    def __str__(self) :
+        return f'{self.telefono} - {self.user}'
+    
 
--- myapp/views.py
+++ myapp/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
-from myapp.models import Canchas, DatosPersona, Horario
+from myapp.models import Canchas, DatosReserva, Telefono, Horario
+from django.contrib.auth.models import User
 from myapp.utils.date_utils import siete_dias
 from django.contrib.auth.decorators import login_required
 import ast
@@ -18,7 +19,15 @@ def futbol_view(request):
     })
 
 def paddel_view(request):
-    return render(request, 'myapp/paddel.html')
+    lista_dias = siete_dias()
+
+    canchas = Canchas.objects.all()
+
+    return render(request, 'myapp/paddel.html', {
+        'lista_dias' : lista_dias,
+        'canchas' : canchas
+    })
+
 
 def horarios_view(request, id):
     lista_horarios = [
@@ -49,18 +58,57 @@ def horarios_view(request, id):
 @login_required
 def confirmar_reserva_view(request, id):
     horario_seleccionado = request.POST.getlist('horario')
-    fecha_seleccionada = request.POST.get('fecha')
+    if not horario_seleccionado:
+        lista_horarios = [
+        '17:00-18:00',
+        '18:00-19:00',
+        '19:00-20:00',
+        '20:00-21:00',
+        '21:00-22:00',
+        '22:00-23:00',
+        '23:00-00:00',
+        ]
+        fecha_seleccionada = request.POST.get('fecha')
+        horarios_queryset = Horario.objects.filter(fecha=fecha_seleccionada, cancha_id=id)
+        
+        horarios_reservados = []
+        for reserva in horarios_queryset:
+            horarios = ast.literal_eval(reserva.horarios)  # ast convierte "[lista]" en [lista] (de string a lista literal)
+            horarios_reservados.append(horarios)
 
-    request.session['horario_seleccionado'] = horario_seleccionado
-    request.session['fecha_seleccionada'] = fecha_seleccionada
+        return render(request, 'myapp/horarios.html', {
+        'id' : id,
+        'fecha_seleccionada' : fecha_seleccionada,
+        'lista_horarios' : lista_horarios, 
+        'horarios_reservados' : horarios_reservados
+        })
+    
+    else:
+        fecha_seleccionada = request.POST.get('fecha')
+        request.session['horario_seleccionado'] = horario_seleccionado
+        request.session['fecha_seleccionada'] = fecha_seleccionada
 
-    return render(request, 'myapp/confirmacion.html', {
-        'id' : id
-    })
+        datos_usuario = request.user
+        datos_telefono = Telefono.objects.filter(user=datos_usuario).first()
+
+        if datos_telefono:
+            telefono = datos_telefono.telefono
+        else:
+            telefono = None
+
+        return render(request, 'myapp/confirmacion.html', {
+            'id' : id,
+            'datos_usuario' : datos_usuario,
+            'telefono' : telefono,
+        })
 
 @login_required
 def confirmar_reserva_2_view(request, id):
     cancha_instancia = Canchas.objects.get(id=id)
+    user_id = request.user.id
+    usuario_instancia = User.objects.get(id=user_id)
+    datos_usuario = request.user
+    actualiza_telefono = Telefono.objects.filter(user=datos_usuario).first()
     
     if request.method == 'POST':
         nombre = request.POST.get('nombre')
@@ -85,15 +133,21 @@ def confirmar_reserva_2_view(request, id):
 
                 horario_instancia = Horario.objects.get(id=horario_id)
 
-                datos_persona = DatosPersona(
-                nombre=nombre,
-                apellido=apellido,
-                telefono=telefono,
-                email=email, 
-                horario=horario_instancia,
-                cancha=cancha_instancia
-                )
-                datos_persona.save()
+                if actualiza_telefono:
+                    actualiza_telefono.telefono = telefono
+                    actualiza_telefono.save()
+                else:
+                    actualiza_telefono = Telefono(
+                        user=usuario_instancia,
+                        telefono=telefono
+                    )
+                    actualiza_telefono.save()
+
+                datos_usuario.first_name = nombre
+                datos_usuario.last_name = apellido
+                datos_usuario.email = email
+                datos_usuario.save()
+
             except Exception as e:
                 print(f'ERROR: {e}')
 
